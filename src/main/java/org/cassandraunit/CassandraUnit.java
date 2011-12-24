27880f630c48e8714fc133b3b552a680498bf40d
Thomas Recloux <thomas.recloux@gmail.com>
2011-12-24 16:22:08 +0100

    Rule creates structure and load test data

--- src/main/java/org/cassandraunit/CassandraUnit.java
+++ src/main/java/org/cassandraunit/CassandraUnit.java
@@ -1,23 +1,38 @@
 package org.cassandraunit;
 
 import me.prettyprint.hector.api.Cluster;
+import me.prettyprint.hector.api.Keyspace;
 import me.prettyprint.hector.api.factory.HFactory;
 
+import org.cassandraunit.dataset.DataSet;
 import org.cassandraunit.utils.EmbeddedCassandraServerHelper;
 import org.junit.rules.ExternalResource;
 
 public class CassandraUnit extends ExternalResource {
 	public Cluster cluster;
+	public Keyspace keyspace;
+
+	private DataSet dataSet;
+
 	public String clusterName = "TestCluster";
 	public String host = "localhost:9171";
+
+	public CassandraUnit(DataSet dataSet) {
+		this.dataSet = dataSet;
+	}
 
 	@Override
 	protected void before() throws Exception {
 		/* start an embedded Cassandra */
 		EmbeddedCassandraServerHelper.startEmbeddedCassandra();
 
+		/* create structure and load data */
+		DataLoader dataLoader = new DataLoader(clusterName, host);
+		dataLoader.load(dataSet);
+
 		/* get hector client object to query data in your test */
 		cluster = HFactory.getOrCreateCluster(clusterName, host);
+		keyspace = HFactory.createKeyspace(dataSet.getKeyspace().getName(), cluster);
 	}
 
 	
--- src/test/java/org/cassandraunit/CassandraStartAndLoadRuleTest.java
+++ src/test/java/org/cassandraunit/CassandraStartAndLoadRuleTest.java
@@ -1,18 +1,26 @@
 package org.cassandraunit;
 
+import static org.cassandraunit.SampleDataSetChecker.assertDataSetLoaded;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.Assert.assertThat;
 
+import org.cassandraunit.dataset.xml.ClassPathXmlDataSet;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class CassandraStartAndLoadRuleTest {
 
 	@Rule
-	public CassandraUnit cassandra = new CassandraUnit();
+	public CassandraUnit cassandra = new CassandraUnit(new ClassPathXmlDataSet("xml/datasetDefaultValues.xml"));
 	
 	@Test
 	public void shouldStartCassandraServer ()  {
 		assertThat(cassandra.cluster, notNullValue());
+	}
+	
+	@Test
+	public void shouldCreateStructureAndLoadData() throws Exception {
+		assertThat(cassandra.keyspace, notNullValue());
+		assertDataSetLoaded(cassandra.keyspace);
 	}
 }
